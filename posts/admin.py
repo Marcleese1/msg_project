e0d89e63b1861e2cf3f8e35b232102a85c3b9a59
Marc <30148234@cityofglacol.ac.uk>
2018-02-18 21:39:43 +0000

    finished message app

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,18 @@
+# posts/admin.py
 from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Post)
+
+class CommentInline(admin.StackedInline):
+    model = models.Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comment)
