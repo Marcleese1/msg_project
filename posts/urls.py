a6fdf86a4049628a1c6438dc7ee7d22faf2d79cd
Marc <30148234@cityofglacol.ac.uk>
2018-02-18 20:31:00 +0000

    added post edit, delete and detail

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name='posts'),
+    path('<int:pk>/edit/',
+         views.PostUpdateView.as_view(), name='post_edit'),  # new
+    path('<int:pk>/',
+         views.PostDetailView.as_view(), name='post_detail'),  # new
+    path('<int:pk>/delete/',
+         views.PostDeleteView.as_view(), name='post_delete'),  # new
 ]
