27088e8e7387f2f5ba284f4d9afe756503d73544
Marc <30148234@cityofglacol.ac.uk>
2018-03-09 14:53:21 +0000

    added ability for authors to only edit own posts

--- posts/models.py
+++ posts/models.py
@@ -24,7 +24,7 @@ class Comment(models.Model):
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE, default=1
     )
 
     def get_absolute_url(self):
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
 # posts/views.py
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.http import Http404
 
 
 from . import models
@@ -33,10 +34,19 @@ class PostDetailView(LoginRequiredMixin, DetailView): # new
 
 
 class PostUpdateView(LoginRequiredMixin, UpdateView): # new
+    #if user or superuser
     model = models.Post
     fields = ['message']
     template_name = 'post_edit.html'
-    login_url = 'login' # new
+    login_url = 'login'
+
+#else call this
+#FUNCTION TO ALLOW ONLY THE PERSON POSTING TO EDIT THEIR POSTS.
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise Http404("You are not allowed to edit this Post")#sllows you to show a 404 page when someone who didnt write the post tried to edit it
+        return super(PostUpdateView, self).dispatch(request, *args, **kwargs)
 
 
 class PostDeleteView(LoginRequiredMixin, DeleteView): # new
